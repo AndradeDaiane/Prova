d2a79f3af7c3acd82f091f6d4e49907e7a744323
AndradeDaiane <daiane.santosasd23@gamail.com>
2025-06-20 20:17:45 -0300

    ok

--- provas/src/main/java/com/example/provas/repository/ProdutoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.provas.repository;
-
-import com.example.provas.model.Produto;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-}
